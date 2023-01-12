efac10dce549600e0dfb69451651902f9a67fa97
fanshiqing <fanshiqing@taiyouxi.cn>
2023-01-12 18:27:05 +0800

    【初始化】占坑提交

---
+++ trunk/Assets/AtmosphericScattering/ASUtils.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public static class ASUtils
+{
+    public static void Dispatch(ComputeShader cs, int kernel, Vector2Int lutSize)
+    {
+        if (cs == null)
+        {
+            Debug.LogWarningFormat("Computer shader for precompute scattering lut is empty");
+            return;
+        }
+        
+        uint threadNumX, threadNumY, threadNumZ;
+        cs.GetKernelThreadGroupSizes(kernel, out threadNumX, out threadNumY, out threadNumZ);
+        cs.Dispatch(kernel, lutSize.x / (int) threadNumX,
+            lutSize.y / (int) threadNumY, 1);
+    }
+}
+
+public enum ASProfilerType
+{
+    Precompute,
+}
